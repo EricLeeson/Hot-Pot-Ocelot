23820e587453f78e0598959a1c1ea8490efedcd0
EricLeeson <ericleesoncs@gmail.com>
2024-10-22 06:06:13 -0700

    Updated enemy movement pattern and added camera movement

--- enemy.gd
+++ enemy.gd
@@ -1,21 +1,16 @@
 extends CharacterBody2D
 
-var player 
-
-func _ready() -> void:
-	player = get_node("/root/Game/Ocelot_Player")
+@onready var player = get_node("/root/Game/Ocelot_Player")
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 50
+	velocity = direction * 30
 	move_and_slide()
 	
 	var animationDirection = get_direction(velocity)
 	
 	if velocity.length() > 0.0:
 		%SlimeAnimation.play(animationDirection)
-	else:
-		%SlimeAnimation.stop()
 	
 func get_direction(velocity) -> String:
 	if velocity.x < 0:
